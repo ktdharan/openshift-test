515b688a781c3f456410d845f30b5584eec8ba25
ktdharan <ktdharan@gmail.com>
2022-04-08 07:40:19 +0530

    Update helloworld.py
    testing trigger

--- helloworld.py
+++ helloworld.py
@@ -8,7 +8,7 @@ app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
     """Say hello"""
-    return 'Hello, world!'
+    return 'Hello, world!, this is superb!!!!'
 
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file. Awesome !!!!!!")
